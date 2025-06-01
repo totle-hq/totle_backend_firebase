5e577c658d84f045e4f2170452d04a9d5c27964d
mohnishsingh yadav <132808264+Mohnishsingh998@users.noreply.github.com>
2025-06-01 13:37:01 +0530

    Revert "Champion/mohnishsingh/cta"

--- src/routes/cta.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { trackCTA } from '../controllers/cta.contoroller.js';
-
-const router = express.Router();
-
-router.post('/cta-tracking', trackCTA);
-
-export default router;
